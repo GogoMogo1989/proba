13e53d57f7cd97515589bfa7232238a19cdb30a8
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-03-11 19:47:10 +0100

    20220311

--- script.js
+++ script.js
@@ -88,29 +88,127 @@ const user1 = {
   isConfirmed: true
 }
 
-const user2 = {
+const user2 = [{
   id: 2136523,
   username: "lajosnagy",
   email: "lajos@gmail.com",
   phone: "303214321",
   isConfirmed: true
-}
+}]
 
-const user3 = {
+const user3 = [{
   id: 8743534,
   username: "akarki99",
   email: "kazmer@gmail.com",
   phone: "206546547",
   isConfirmed: false
+}]
+
+let newUser3=user3.map(newArray = (user123) => {
+  let b={name: user123.username,
+    email: user123.email,
+    phone: user123.phone}
+return b
+})
+
+
+const numbers = ["a", "a", "b", "b", "b", "a", "c", "c", "c", "b"]
+
+let count={}
+
+numbers.forEach(item =>{
+  if(count[item]){
+    count[item]++
+  }else{
+    count[item]=1
+  }
+  return item
+})
+
+
+
+
+
+/* console.log(count) */
+
+
+let list1 = [
+  { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+  { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+  { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+];
+
+
+      let python=list1.filter(people)
+
+      function people(value){
+        return value.language === "Python"
+      }
+
+      let python1=""
+
+      for(b of python){
+        python1+=(b.firstName+", "+b.country)
+      }
+
+console.log(python1)
+
+
+/* ----------------------------------------feladat határ------------------------- */
+
+var list2 = [
+  { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+  { firstName: 'Odval', lastName: 'F.', country: 'Mongolia', continent: 'Asia', age: 33, language: 'Python' },
+  { firstName: 'Emilija', lastName: 'S.', country: 'Lithuania', continent: 'Europe', age: 19, language: 'Python' },
+  { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+];
+
+function senior(list){
+
+  let newList = list.reduce(oldest)
+  
+  function oldest(accumulator, value){
+
+      if(accumulator>value.age){
+      return accumulator
+      }else{
+      return value.age
+      }
+
+  }
+
+  let beka = list.filter(milter)
+
+  function milter(value){
+
+    return value.age === newList
+
+  }
+
+  /* console.log(beka) */
+
 }
 
+/* senior(list2) */
 
-function getUserIdentifiers(user3) {
-  // todo
-  console.log(user3.id)
- 
-  
+
+/* --------------------------------------következő feladat------------------------------------------- */
+
+let list3 = [
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+];
+
+
+function isRubyComing(list) {
+  // thank you for checking out my kata :)
+  let b = list.some(kutya)
+
+  function kutya(value){
+    return value.language ==="Ruby"
   }
-  
+return(b)
+}
 
-
+isRubyComing(list3)
